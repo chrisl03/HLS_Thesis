11ebe0a63d58974f6a41dfb9b72f0ddf4ea5356f
aimilefth <aimilefth@github.com>
2026-05-21 18:49:29 +0300

    Changes so that HLS runs. run_settings.sh changed for server

--- build/build.tcl
+++ build/build.tcl
@@ -1,35 +1,22 @@
-# build.tcl (auto-generated)
-
-# Create a project
 open_project -reset hls_project
-# Add design files
-add_files src/gemm.cpp -cflags "-std=c++14 -DALLOW_EMPTY_HLS_STREAM_READS"
-add_files -tb src/gemm_csim_tb.cpp -cflags "-std=c++14 -DALLOW_EMPTY_HLS_STREAM_READS"
 
-# Set the top-level function
-set_top gemm
+add_files src/SODA_lcs_generic_k.cpp -cflags "-std=c++14 -DALLOW_EMPTY_HLS_STREAM_READS"
+add_files -tb src/SODA_lcs_generic_k_tb.cpp -cflags "-std=c++14 -DALLOW_EMPTY_HLS_STREAM_READS"
 
-# ########################################################
-# Create a solution
+set_top architecture_top_level
 
-open_solution -flow_target vitis -reset "gemm"
+open_solution -flow_target vitis -reset "architecture_top_level"
 
-# Device Selection: vck190
-set_part {xcvc1902-vsva2197-2MP-e-S}
+set_part {xcu55c-fsvh2892-2L-e}
 
-create_clock -period 300MHz -name default
+create_clock -period 4.000 -name default
 
-# Disabling automatic pipelining of loops
 config_compile -pipeline_loops 0
 config_interface -m_axi_conservative_mode=0
 config_compile -enable_auto_rewind
 config_rtl -add_register_in_block_condition=false
-
-# Disable bugged (?) srl mapping
+config_compile -unsafe_math_optimizations
 config_storage fifo -impl auto -auto_srl_max_bits 0 -auto_srl_max_depth 0
-# config_compile -unsafe_math_optimizations
-
-# Disabling automatic partition
 config_array_partition -throughput_driven off
 config_array_partition -complete_threshold 1
 
@@ -38,3 +25,12 @@ csynth_design
 cosim_design -trace_level all -enable_dataflow_profiling
 
 exit
+
+#vitis_hls -f run_hls.tcl
+#vitis_hls -p cong_stencil_project_2
+
+#call C:\Xilinx2025.2\2025.2\Vitis\settings64.bat
+#cd /d C:\GitHub\HLS_Thesis
+#vitis-run --mode hls --tcl run_hls.tcl
+
+#%APPDATA%/Xilinx/HLS_init.tcl
--- build/run_settings.sh
+++ build/run_settings.sh
@@ -1,4 +1,4 @@
-source /opt/Xilinx/Vitis/2023.2/settings64.sh
+source /tools/Xilinx/Vitis/2023.2/settings64.sh
 source /opt/xilinx/xrt/setup.sh
 #export PLATFORM_REPO_PATHS=/opt/xilinx/platforms/
 export PRJ_PATH=$(pwd)
