133df598de270f2fc03650bf9bc02adda0e3f408
chrisl03 <cliouskos@gmail.com>
2026-05-23 01:05:48 +0300

    source-host restructuring

---
+++ soda_host/host_visible.h
@@ -0,0 +1,26 @@
+// host_visible.h
+#ifndef HOST_VISIBLE_H
+#define HOST_VISIBLE_H
+
+// Helper macros
+#define MIN(a, b)  ((a) < (b) ? (a) : (b))
+#define MAX(a, b)  ((a) > (b) ? (a) : (b))
+#define CEIL_DIV(X, Y) (((X) + (Y) - 1) / (Y))
+
+// basic params
+#define SODA_K 8
+#define SODA_ROWS 16
+#define SODA_COLS 1024
+
+// derived parameters 
+#define SODA_TOTAL_PIXELS (SODA_ROWS * SODA_COLS)
+#define SODA_KERNEL_ROWS (SODA_ROWS - 2)
+#define SODA_KERNEL_COLS (SODA_COLS - 2)
+#define SODA_KERNEL_ITER (SODA_KERNEL_ROWS * SODA_KERNEL_COLS)
+
+// amt of bursts of 16 floats - buffer sizes
+#define SODA_BURSTS_IN CEIL_DIV(SODA_TOTAL_PIXELS, 16)
+
+#define SODA_TOTAL_PACKETS_OUT (CEIL_DIV(SODA_KERNEL_ITER, SODA_K) + 64)
+
+#endif //HOST_VISIBLE_H
